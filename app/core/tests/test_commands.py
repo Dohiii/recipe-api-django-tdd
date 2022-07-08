9c8f4b143326a00e3322baced2615230689daaf7
Dohiii <77291884+Dohiii@users.noreply.github.com>
2022-07-08 20:58:06 +0200

    added mock tests

--- app/core/tests/test_commands.py
+++ app/core/tests/test_commands.py
@@ -3,7 +3,7 @@ Test custom Django management commands.
 """
 from unittest.mock import patch
 
-from psycopg2 import OperationalError as Psycopg2Error
+from psycopg2 import OperationalError as Psycopg2OpError
 
 from django.core.management import call_command
 from django.db.utils import OperationalError
@@ -16,7 +16,7 @@ class CommandTests(SimpleTestCase):
 
     def test_wait_for_db_ready(self, patched_check):
         """Test waiting for database if database ready. """
-        patched_check.returned_value = True
+        patched_check.return_value = True
 
         call_command('wait_for_db')
 
@@ -27,7 +27,7 @@ class CommandTests(SimpleTestCase):
         """Test waiting for database when getting OperationalError."""
 
         """two times Mock is raising Psycopg2Error and 3 times OperationalError"""
-        patched_check.side_effects = [Psycopg2Error] * 2 + \
+        patched_check.side_effect = [Psycopg2OpError] * 2 + \
             [OperationalError] * 3 + [True]
 
         call_command('wait_for_db')
