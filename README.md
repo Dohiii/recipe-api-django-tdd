72ae80da6c660069fc8504d5ee575fcae1311bae
Dohiii <77291884+Dohiii@users.noreply.github.com>
2022-07-08 21:54:18 +0200

    added mock tests

--- README.md
+++ README.md
@@ -13,9 +13,12 @@ docker-compose run --rm app sh -c "flake8"
 docker-compose run --rm app sh -c "django-admin startproject app ."
 docker-compose run --rm app sh -c "django-admin startapp core"
 
-docker-compose run --rm app sh -c "python manage.py test"
 
+
+docker-compose run --rm app sh -c "python manage.py test"
+docker-compose run --rm app sh -c "python manage.py flake8"
 
 Custom django commands:
-slowing down the django to wait for database
+slowing down the django to wait for database:
+docker-compose run --rm app sh -c "python manage.py wait_for_db"
 
--- app/core/tests/test_commands.py
+++ app/core/tests/test_commands.py
@@ -20,17 +20,17 @@ class CommandTests(SimpleTestCase):
 
         call_command('wait_for_db')
 
-        patched_check.assert_called_once_with(database=['default'])
+        patched_check.assert_called_once_with(databases=['default'])
 
     @patch('time.sleep')
     def test_wait_for_db_delay(self, patched_sleep, patched_check):
         """Test waiting for database when getting OperationalError."""
 
-        """two times Mock is raising Psycopg2Error and 3 times OperationalError"""
+        # two times Mock is raising Psycopg2Error and 3 times OperationalError
         patched_check.side_effect = [Psycopg2OpError] * 2 + \
             [OperationalError] * 3 + [True]
 
         call_command('wait_for_db')
 
         self.assertEqual(patched_check.call_count, 6)
-        patched_check.assert_called_with(database=['default'])
+        patched_check.assert_called_with(databases=['default'])
