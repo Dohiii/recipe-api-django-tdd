8fa7d3cb48ad1b039ffc86ab4640e1ee750c4410
Dohiii <77291884+Dohiii@users.noreply.github.com>
2022-07-15 13:30:51 +0200

    added documents and user serializer

--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -57,10 +57,9 @@ class PublicUserApiTests(TestCase):
             'password': '123',
             'name': 'Test Name'
         }
-        create_user(**payload)
+
         res = self.client.post(CREATE_USER_URL, payload)
         self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
         user_exists = get_user_model().objects.filter(
                                       email=payload['email']).exists()
         self.assertFalse(user_exists)
-
